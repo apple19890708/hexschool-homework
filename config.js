3439a236ff74d85502994ab62977e61d2b2c8724
Tommy <Tommychen@applyeasypro.com>
2021-06-04 18:49:11 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+
+// const deviceName = 'iPhone';
+const apiUrl = 'https://vue3-course-api.hexschool.io';
+const apiPath = 'orangetommy';
+
+export {
+  apiUrl,
+  apiPath
+};
+
+export function fn() {
+  console.log('這是外部匯入的具名函式');
+};
